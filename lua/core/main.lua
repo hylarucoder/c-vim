ac901adbd0225d18ffe2bb0efd803cfe2a9c45e5
twocucao <twocucao@gmail.com>
2020-11-14 03:10:20 +0800

    better-arch

--- lua/core/main.lua
+++
@@ -1,6 +0,0 @@
-local vim = vim
-vim.g.mapleader = ","
-vim.g.maplocalleader = ","
-local global = require("core.global")
-local basic = require("core.basic")
-local plugins = require("core.plugins")
