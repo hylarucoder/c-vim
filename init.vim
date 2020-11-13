95ef5f8e0c3a9d8dc961570d8b90fe03036c2733
twocucao <twocucao@gmail.com>
2020-11-13 23:29:08 +0800

    better group

--- init.vim
+++ init.vim
@@ -17,12 +17,6 @@ let s:home = fnamemodify(resolve(expand('<sfile>:p')), ':h')
 " 定义一个命令用来加载文件
 command! -nargs=1 LoadScript exec 'so '.s:home.'/'.'<args>'
 
-" 将 vim-init 目录加入 runtimepath
-exec 'set rtp+='.s:home
-
-" 将 ~/.vim 目录加入 runtimepath (有时候 vim 不会自动帮你加入）
-set rtp+=~/.vim
-
 "----------------------------------------------------------------------
 " 模块加载
 "----------------------------------------------------------------------
--- lua/core/main.lua
+++ lua/core/main.lua
@@ -126,80 +126,88 @@ end
 
 -- Example usageplug {'julia', { g = {default_julia_version = '1.1'} }}
 plug("~/.vim/bundles", {
-  -- 开始画面
-  "mhinz/vim-startify",
-  -- 菜单
-  "skywind3000/vim-quickui",
-  -- 自定义菜单/命令
-  "liuchengxu/vim-which-key",
-  -- popup
-  "nvim-lua/popup.nvim",
-  -- lua utils
-  "nvim-lua/plenary.nvim",
-  -- telescope.nvim
-  "nvim-lua/telescope.nvim",
+  -- >>>> Appearance <<<< --
   -- icon font
   "kyazdani42/nvim-web-devicons",
+  -- 开始画面
+  "mhinz/vim-startify",
+  -- 自定义菜单/命令
+  "skywind3000/vim-quickui",
+  "liuchengxu/vim-which-key",
+
+  -- lua utils
+  "nvim-lua/popup.nvim",
+  "nvim-lua/plenary.nvim",
+  -- fuzzfinder
+  "nvim-lua/telescope.nvim",
   -- file explorer
   "kyazdani42/nvim-tree.lua",
+  -- tagbar
+  "liuchengxu/vista.vim",
   -- status bar
   "itchyny/lightline.vim",
-  -- Completer
+  -- Completor
   "nvim-lua/completion-nvim",
   {"aca/completion-tabnine", ["do"] = "./install.sh"}, -- 侧边栏增强
   "SirVer/ultisnips",
   "honza/vim-snippets", -- 侧边栏增强
-  -- >> 文本技
-  -- 用 v 选中一个区域后，+/- 按分隔符扩大/缩小选区
-  "terryma/vim-expand-region",
-  -- 对齐
-  "junegunn/vim-easy-align",
+  "asins/vim-dict", 
+  -- 配对括号和引号自动补全
+  "Raimondi/delimitMate",
+  -- language server
+  "neovim/nvim-lspconfig",
+  "nvim-lua/diagnostic-nvim",
+
+  -- >>>> 移动增强 <<<< --
+  "justinmk/vim-sneak",
+  "rhysd/accelerated-jk",
+  -- 全文快速移动，<leader><leader>f{char} 即可触发
+  "easymotion/vim-easymotion", 
+
+  -- >>>> 文本技 <<<< --
   -- Diff 增强，支持 histogram / patience 等更科学的 diff 算法
   "chrisbra/vim-diff-enhanced",
-  -- " 基础插件：提供让用户方便的自定义文本对象的接口
+  -- +/- 扩大/缩小选区
+  "terryma/vim-expand-region",
+  -- >>>> textobj <<<< --
+  -- 基础插件：提供让用户方便的自定义文本对象的接口
   "kana/vim-textobj-user",
-  -- " indent 文本对象：ii/ai 表示当前缩进，vii 选中当缩进，cii 改写缩进
+  -- indent 文本对象：ii/ai 表示当前缩进，vii 选中当缩进，cii 改写缩进
   "kana/vim-textobj-indent",
-  -- " 语法文本对象：iy/ay 基于语法的文本对象
+  -- 语法文本对象：iy/ay 基于语法的文本对象
   "kana/vim-textobj-syntax",
-  -- " 函数文本对象：if/af 支持 c/c++/vim/java
+  -- 函数文本对象：if/af 支持 c/c++/vim/java
   {
     "kana/vim-textobj-function",
     ["for"] = {"c", "cpp", "vim", "java"}
   },
-  -- " 参数文本对象：i,/a, 包括参数或者列表元素
+  -- 参数文本对象：i,/a, 包括参数或者列表元素
   "sgur/vim-textobj-parameter",
-  -- " 提供 python 相关文本对象，if/af 表示函数，ic/ac 表示类
+  -- 提供 python 相关文本对象，if/af 表示函数，ic/ac 表示类
   {"bps/vim-textobj-python", ["for"] = {"python"}},
-  -- " 提供 uri/url 的文本对象，iu/au 表示
+  -- 提供 uri/url 的文本对象，iu/au 表示
   "jceb/vim-textobj-uri",
   "tpope/vim-abolish",
-  -- syntax enhanced
+
+  -- >>>> Syntax Highlight <<<< --
+  "luochen1990/rainbow", 
   "sheerun/vim-polyglot",
-  -- linter
-  "w0rp/ale",
-  "psf/black",
   "norcalli/nvim-colorizer.lua",
-  "liuchengxu/vista.vim",
-  "neovim/nvim-lspconfig",
-  "nvim-lua/diagnostic-nvim",
+  -- >>>> Linter <<<< --
+  "w0rp/ale",
+  -- >>>> Formatter <<<< --
+  "junegunn/vim-easy-align",
+  "psf/black",
+
+  -- >>>> VSC GIT <<<< --
   "tpope/vim-fugitive",
   "mhinz/vim-signify", 
-  -- 移动增强
-  "rhysd/accelerated-jk",
-  "justinmk/vim-sneak",
-  -- 全文快速移动，<leader><leader>f{char} 即可触发
-  "easymotion/vim-easymotion", 
-  -- 给不同语言提供字典补全，插入模式下 c-x c-k 触发
-  "asins/vim-dict", 
-  -- 括号
-  "luochen1990/rainbow", 
-  -- 配对括号和引号自动补全
-  "Raimondi/delimitMate",
-  -- lua support
+
+  -- lua
   "tjdevries/nlua.nvim",
   "euclidianAce/BetterLua.vim",
   {"tbastos/vim-lua", ["for"] = {"lua"}},
   "andrejlevkovitch/vim-lua-format", -- " rust 语法增强
+  -- rust
   {"rust-lang/rust.vim", ["for"] = {"rust"}}
 })
