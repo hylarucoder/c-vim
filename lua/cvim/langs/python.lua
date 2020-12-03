bba29bdc3e04977dcfaef8c958a06ccae9bdd3c1
twocucao <twocucao@gmail.com>
2020-12-03 09:44:07 +0800

    add init

--- lua/cvim/langs/python.lua
+++ lua/cvim/langs/python.lua
@@ -1,4 +1,7 @@
 local lsp = require("lspconfig")
 local utils = require("cvim.langs.utils")
 local on_attach_vim = utils.on_attach_vim
-lsp.pyls.setup {on_attach = on_attach_vim, settings = {pyls = {plugins = {pycodestyle = {enabled = false}}}}}
+lsp.pyls.setup {
+  on_attach = on_attach_vim,
+  settings = {pyls = {plugins = {pycodestyle = {enabled = false}, pylint = {enabled = false}}}}
+}
